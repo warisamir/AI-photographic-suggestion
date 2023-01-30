e25188ef8baeccfde979619c0608ac8e5cafebb6
WarisAmir <warisamir1918@gmail.com>
2023-01-30 19:45:52 +0530

    Add files via upload

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+// import { Form } from 'react-router-dom'
+// import { preview } from '../assets'
+
+const FormField = ({LabelName,type,name,placeholder,value,handleChange,isSurpriseMe ,handleSurpriseMe}) => (
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+      <label htmlFor={name}
+      className='block text-sm font-medium text-gray-900' >
+      {LabelName}</label>
+      {isSurpriseMe && (
+        <button type="button"
+        onClick={handleSurpriseMe}
+        className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black">
+        Surprise me
+        </button>
+      )}
+      </div>
+      <input 
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required className='bg-grey-50 border border-gray-300 text-grey-900
+        text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4639ff] outline-none block w-full p-3'
+      />
+    </div>
+  )
+
+
+export default FormField
