3bbae428297d774e35a124972a79edddb87ed6e7
WarisAmir <warisamir1918@gmail.com>
2023-05-19 14:21:25 +0530

    Update connect.js

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,12 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const connectDB=(url)=>{
-    mongoose.set('strictQuery',true);//it isusefull when working set functionality
-    mongoose.connect(url)
-    .then(()=>console.log('MongoDb connected'))
-    .catch((err)=>console.log(err))
-}
-export default connectDB
+const connectDB = (url) => {
+  mongoose.set('strictQuery', true);
+  mongoose.connect(url)
+    .then(() => console.log('connected to mongo'))
+    .catch((err) => {
+      console.error('failed to connect with mongo');
+      console.error(err);
+    });
+};
+
+export default connectDB;
 
 
 
