812e9ed649a4da04d4411ec2a6238c1670645bb9
WarisAmir <warisamir1918@gmail.com>
2023-05-19 14:08:17 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://ai-website.onrender.com/api/v1/post', {
+      const response = await fetch('https://ai-photogallary.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
